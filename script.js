45e76c34768558db3f7b07fb9454aa50bb69f4dc
Dávid <141168015+Ha-Khu@users.noreply.github.com>
2023-08-24 14:44:01 +0200

    Made squares for box

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-let squares_DOM = document.getElementById("squares-container")
+let squares_DOM = document.querySelector(".squares-container")
 
+squares_DOM.style.gridTemplateColumns = "repeat(16, 1fr)"
+squares_DOM.style.gridTemplateRows = "repeat(16, 1fr)"
+
+for (let i = 0; i < 256; i++) {
+    let square = document.createElement("div")
+    squares_DOM.insertAdjacentElement("beforeend", square)
+}
