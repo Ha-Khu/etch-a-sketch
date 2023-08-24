93270d1e04135f0a0a51d5afb0c4dcd91b828686
Dávid <141168015+Ha-Khu@users.noreply.github.com>
2023-08-24 18:07:17 +0200

    Added getSize function for choosing size of squares

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 let squares_DOM = document.querySelector(".squares-container")
+let changeSize = document.querySelector(".size")
 
-squares_DOM.style.gridTemplateColumns = "repeat(16, 1fr)"
-squares_DOM.style.gridTemplateRows = "repeat(16, 1fr)"
 
-for (let i = 0; i < 256; i++) {
+changeSize.addEventListener("click", function(){
+    let number = getSize()
+    count(number)
+})
+
+
+function count (number) {
+squares_DOM.style.gridTemplateColumns = `repeat(${number}, 1fr)` 
+squares_DOM.style.gridTemplateRows = `repeat(${number}, 1fr)` 
+
+let size = number * number
+for (let i = 0; i < size; i++) {
     let square = document.createElement("div")
+    square.classList.add("squares")
+    square.addEventListener("mouseover", changeColor)
     squares_DOM.insertAdjacentElement("beforeend", square)
 }
+
+}
+
+count()
+
+function getSize () {
+    let input = prompt("Choose size")
+    return input
+    
+}
+
+
+function changeColor() {
+    this.style.backgroundColor = "black"
+}
+
